aaa93d5938819d72c79bd0d713456f7c1bcf946b
Tanish Bhardwaj <tanishbhardwaj21@gmail.com>
2026-08-27 20:06:46 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool contains_the_node(TreeNode* root, TreeNode* p)
+    {
+        if(root==nullptr)
+            return false;
+        if(root->val == p->val)
+            return true;
+        bool a = contains_the_node(root->left,p);
+        bool b = contains_the_node(root->right,p);
+        if(a==false && b==false)
+            return false;
+        return true;
+    }
+    void dfs(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& node)
+    {
+        if(root!=nullptr)
+        {
+            if(root->val==p->val)
+            {
+                if(q->val>p->val && contains_the_node(root->right, q)== true)
+                {
+                    node = root;
+                    return;
+                }
+                if(q->val<p->val && contains_the_node(root->left, q)==true)
+                {
+                    node = root;
+                    return;
+                }
+            }
+            if(root->val==q->val)
+            {
+                if(p->val>q->val && contains_the_node(root->right, p)== true)
+                {
+                    node = root;
+                    return;
+                }
+                if(p->val<q->val && contains_the_node(root->left, p)==true)
+                {
+                    node = root;
+                    return;
+                }
+            }
+
+            if(root->val < min(p->val, q->val))
+                dfs(root->right,p,q,node);
+
+            else if(root->val > max(p->val, q->val))
+                dfs(root->left, p, q, node);
+            
+            else{
+                node = root;
+                return;
+            }
+        }
+        return;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* node = root;
+        dfs(root,p,q,node);
+        return node;
+
+        
+    }
+};
