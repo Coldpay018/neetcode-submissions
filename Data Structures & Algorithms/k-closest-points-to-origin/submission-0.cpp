901501f0da21b1a58f85f03312a50c6dbb68a99d
Tanish Bhardwaj <tanishbhardwaj21@gmail.com>
2026-09-06 16:56:51 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    struct Compare {
+        bool operator()(const vector<int>& a, const vector<int>& b)
+        {
+            return sqrt(a[0]*a[0] + a[1]*a[1]) > sqrt(b[0]*b[0] + b[1]*b[1]);
+        }
+    };
+    priority_queue< vector<int>, vector<vector<int>>, Compare> pq;
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        for(vector<int> i : points)
+        {
+            pq.push(i);
+        }
+
+        vector<vector<int>> ans;
+        int count = 0;
+
+        while(count!=k)
+        {
+            vector<int> temp = pq.top();
+            ans.push_back(temp);
+            pq.pop();
+            count++;
+        }
+
+        return ans;
+
+    }
+};
