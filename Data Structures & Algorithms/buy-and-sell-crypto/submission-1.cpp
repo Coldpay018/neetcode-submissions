c64e8a46df3d46c143787f17878bd6e0d792ab4a
Tanish Bhardwaj <tanishbhardwaj21@gmail.com>
2026-09-13 10:53:47 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int max(vector<int>& arr)
+    {
+        int n = arr.size();
+        int max = INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]>max)
+                max = arr[i];
+        }
+        return max;
+    }
+    int min(vector<int>& arr, int start, int end)
+    {
+        int min = INT_MAX;
+        for(int i=start; i<=end; i++)
+        {
+            if(arr[i]<min)
+                min = arr[i];
+        }
+        return min;
+    }
+    int maxProfit(vector<int>& prices) {
+        int buy_day = 0;
+        vector<int> profits;
+        for(int i=0; i<prices.size(); i++)
+        {
+            buy_day = min(prices,0,i);
+            profits.push_back(prices[i] - buy_day);
+        }
+        return max(profits);
+    }
+};
