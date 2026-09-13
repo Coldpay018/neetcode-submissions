a1e2de1737c0c35c1dbaf2d33d23e8e80192102a
Tanish Bhardwaj <tanishbhardwaj21@gmail.com>
2026-09-13 13:20:39 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy_day = prices[0];
+        int profit = 0;
+
+        for(int i=0; i<prices.size(); i++)
+        {
+            buy_day = min(buy_day, prices[i]);
+            profit = max(profit, prices[i] - buy_day);
+        }
+        return profit;
+
+        
+    }
+};
